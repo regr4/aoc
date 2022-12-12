662d9ad0b7732c2370acd2fc38c56787aeed9db8
regr4 <60316183+regr4@users.noreply.github.com>
2022-12-12 17:12:38 +0100

    switched to A* and queues for day 12, making it much faster.

--- 2022/day12/solution.py
+++ 2022/day12/solution.py
@@ -2,6 +2,8 @@
 
 from typing import Callable, Dict, List, Set, Tuple, Union
 from collections import defaultdict
+from queue import PriorityQueue
+
 
 with open("input") as f:
     inp = f.read()
@@ -35,97 +37,74 @@ for i, r in enumerate(inp.split()):
             end = (i, j)
 
 
-# I tried to implement A*, but it gave me the wrong answer. Not sure what I did wrong, the heuristic seems to be fine. Maybe priorityqueue is not doing what i think it is?
-"""
-from queue import PriorityQueue
-
 # translated from wikipedia pseudocode
-def a_star(g: Graph) -> Path:
+def a_star(g: Graph, start: Node, end: Union[Node, Callable[[Node], bool]]) -> Path:
+    endp = end if callable(end) else lambda x: x == end
+
     def heuristic(n: Node) -> float:
+        # can't really find a useful heuristic if we don't know what the end is going to be
+        # so just revert to dijkstra's (which is essentially A* with the heuristic always zero)
+        if callable(end):
+            return 0
+
         (xn, yn) = n
         (xe, ye) = end
+        # taxicab metric.
+        # admissible (always underestimates), so we're guaranteed an optimal solution.
         return abs(xn - xe) + abs(yn - ye)
-        # return min(abs(xn - xe), abs(yn - ye))
 
     def reconstructPath(n: Node) -> Path:
         path = [n]
         prev = n
         while (prev := came_from[prev]) != start:
             path.append(prev)
+        path.append(start)
         return list(reversed(path))
 
+    # priority queue of nodes. stores tuples (f-score, node) to get the order right
     q: PriorityQueue = PriorityQueue()
-    q.put(start)
+    q.put((heuristic(start), start))
 
     came_from: Dict[Node, Node] = {}
 
-    g_score = defaultdict(lambda: float("inf"))
+    g_score: Dict[Node, float] = defaultdict(lambda: float("inf"))
     g_score[start] = 0
-    f_score = defaultdict(lambda: float("inf"))
+    f_score: Dict[Node, float] = defaultdict(lambda: float("inf"))
     f_score[start] = heuristic(start)
 
     while not q.empty():
-        current = q.get()
-        if current == end:
+        _, current = q.get()
+        if endp(current):
             return reconstructPath(current)
 
         for neighbour in g[current]:
-            # print(f"{neighbour = }")
             tentative_g_score = g_score[current] + 1  # weights are all 1
             if tentative_g_score < g_score[neighbour]:
                 came_from[neighbour] = current
                 g_score[neighbour] = tentative_g_score
                 f_score[neighbour] = tentative_g_score + heuristic(neighbour)
                 if neighbour not in q.queue:
-                    q.put(neighbour)
+                    q.put((f_score[neighbour], neighbour))
 
-    OhNo()
+    raise Exception("Did not find a path.")
 
 
-def showPath():
+def showPath(path: Path, start: Node, end: Node):
     output = defaultdict(lambda: ".")
-    output[start] = "S"
-    output[end] = "E"
-    for i in range(len(res) - 1):
-        (px, py) = res[i]
-        (nx, ny) = res[i + 1]
-        output[res[i]] = {(-1, 0): "v", (1, 0): "^", (0, -1): ">", (0, 1): "<"}[
+    for i in range(len(path) - 1):
+        (px, py) = path[i]
+        (nx, ny) = path[i + 1]
+        output[path[i]] = {(-1, 0): "v", (1, 0): "^", (0, -1): ">", (0, 1): "<"}[
             (px - nx, py - ny)
         ]
+
+    output[start] = "S"
+    output[end] = "E"
 
     for i in range(len(heights)):
         for j in range(len(heights[0])):
             print(output[(i, j)], end="")
         print()
-
-res = a_star(graph)
-"""
-
-
-def dijkstra(g: Graph, start: Node, end: Union[Node, Callable[[Node], bool]]) -> float:
-    if not callable(end):
-        endp = lambda x: x == end
-    else:
-        endp = end
-
-    unvisited = set(g.keys())
-    tentative_distance: Dict[Node, float] = defaultdict(lambda: float("inf"))
-    tentative_distance[start] = 0
-    curr_node = start
-    while not endp(curr_node):
-        for n in g[curr_node]:
-            if n in unvisited:
-                tentative_distance[n] = min(
-                    tentative_distance[n], tentative_distance[curr_node] + 1
-                )
-        unvisited.remove(curr_node)
-
-        curr_node = next(iter(unvisited))
-        for n in unvisited:
-            if tentative_distance[n] <= tentative_distance[curr_node]:
-                curr_node = n
-
-    return tentative_distance[curr_node]
 
 
 def init_graph(pred: Callable[[int, int], bool]) -> Graph:
@@ -141,11 +120,20 @@ def init_graph(pred: Callable[[int, int], bool]) -> Graph:
 
 
 # part 1
-graph = init_graph(lambda self_height, other_height: other_height <= self_height + 1)
-res = dijkstra(graph, start, end)
-print(f"part 1: {res}")
+res = a_star(
+    init_graph(lambda self_height, other_height: other_height <= self_height + 1),
+    start,
+    end,
+)
+print(f"part 1: {len(res)-1}")
+# showPath(res, start, end)
+
 
 # part 2
-graph = init_graph(lambda self_height, other_height: self_height <= other_height + 1)
-res = dijkstra(graph, end, lambda pos: heights[pos[0]][pos[1]] == 0)
-print(f"part 2: {res}")
+res = a_star(
+    init_graph(lambda self_height, other_height: self_height <= other_height + 1),
+    end,
+    lambda pos: heights[pos[0]][pos[1]] == 0,
+)
+print(f"part 2: {len(res)-1}")
+# showPath(res, end, res[-1])

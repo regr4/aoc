5704f15ed91a701376c5ebf81a543fb5cca36889
regr4 <60316183+regr4@users.noreply.github.com>
2022-12-15 20:39:31 +0100

    cleaned up day 15

--- 2022/day15/solution.py
+++ 2022/day15/solution.py
@@ -1,7 +1,6 @@
 """day n"""
 
 # common imports
-import itertools
 import re
 
 with open("input") as f:
@@ -9,6 +8,7 @@ with open("input") as f:
 
 YVALUE = 2000000
 MAX_COORD = 4000000
+
 
 def manhattan_dist(v1, v2):
     return sum(abs(xi - yi) for (xi, yi) in zip(v1, v2))
@@ -62,8 +62,6 @@ while True:
     if ix >= len(intervals) - 1:
         break
 
-# print(intervals)
-
 res = 0
 for (x0, x1) in intervals:
     res += x1 - x0 + 1
@@ -73,36 +71,11 @@ res -= len(set(b for (s, b) in coords if b[1] == YVALUE))
 print(f"part 1: {res}")
 
 # part 2
-def good_pos(x, y):
-    for s, d in sensors_dists.items():
-        if manhattan_dist((x, y), s) <= d:
-            return False
-
-    return True
-
-
-# for s1, s2 in itertools.product(sensors_dists, sensors_dists):
-#     if s1 == s2:
-#         continue
-#     if (sum(s1) - sum(s2)) % 2 != 0:
-#         continue
-#     # print(s1, s2)
-
-#     # find manhattan intersection?
-
-# the UNIQUE beacon must be on (/near?) the intersection of two 'circles' around sensors.
-# or maybe bisect: can you quickly check a whole area is covered?
-# divide into tiles and check which are entirely covered by a single beacon?
-# let's try bisection
-
-
 res = 0
-
 tiles_to_check = [((0, 0), (MAX_COORD, MAX_COORD))]
-# tiles_to_check = [((0, 0), (20, 20))]
-
 tiles_excluded = 0
 i = 0
+
 while True:
     i += 1
     if i % 10000 == 0:
@@ -114,18 +87,13 @@ while True:
 
     ((sx, sy), (ex, ey)) = tiles_to_check.pop()
 
-    put_smaller = True
-    for s, d in sensors_dists.items():
-        if (
-            max(
-                *[
-                    manhattan_dist(s, v)
-                    for v in [(sx, sy), (sx, ey), (ex, sy), (ex, ey)]
-                ]
-            )
-            <= d
-        ):
-            put_smaller = False
+    # is there any sensor that eliminates the entire tile? then False.
+    # faster than nested for loops, apparently
+    # eliminated itertools.product because this seems to be slightly faster
+    put_smaller = not any(
+        all(manhattan_dist(s, v) <= d for v in [(sx, sy), (sx, ey), (ex, sy), (ex, ey)])
+        for s, d in sensors_dists.items()
+    )
 
     if not put_smaller:
         tiles_excluded += (ex - sx) * (ey - sy)
@@ -136,11 +104,7 @@ while True:
             res = sx * MAX_COORD + sy
             break
 
-        if sx > ex:
-            pass
-        elif sy > ey:
-            pass
-        else:
+        if sx <= ex and sy <= ey:
             midpoint_x = (sx + ex) // 2
             midpoint_y = (sy + ey) // 2
 
@@ -148,47 +112,5 @@ while True:
             tiles_to_check.append(((sx, midpoint_y + 1), (midpoint_x, ey)))
             tiles_to_check.append(((midpoint_x + 1, sy), (ex, midpoint_y)))
             tiles_to_check.append(((midpoint_x + 1, midpoint_y + 1), (ex, ey)))
-
-# coord_max = 4000001
-# # coord_max = 20
-# steps = 100
-
-# tiles = [[False] * (steps + 1) for _ in range(steps + 1)]
-
-# step = coord_max // steps
-# print(f"{coord_max = }\n{steps = }\n{step = }")
-# for i in range(0, coord_max, step):
-#     for j in range(0, coord_max, step):
-#         i1 = i + step - 1
-#         j1 = j + step - 1
-#         for s, d in sensors_dists.items():
-#             if (
-#                 max(
-#                     *[
-#                         manhattan_dist(s, v)
-#                         for v in [(i, j), (i, j1), (i1, j), (i1, j1)]
-#                         # typo i1 instead of j1 cost me like 20 mins...
-#                     ]
-#                 )
-#                 <= d
-#             ):
-#                 tiles[i // step][j // step] = True
-#                 break
-
-# # for row in tiles:
-#     # for col in row:
-#         # if col: print("1", end="")
-#         # else: print("0", end="")
-#     # print("")
-
-# tiles_to_inspect = []
-
-# for i, row in enumerate(tiles):
-#     for j, col in enumerate(row):
-#         if not col:
-#             tiles_to_inspect.append((i, j))
-
-# print(tiles_to_inspect)
-# print(len(tiles_to_inspect))
 
 print(f"part 2: {res}")

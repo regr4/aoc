3da1f35074e7e8da0b0c61fb98afd216f102c722
regr4 <60316183+regr4@users.noreply.github.com>
2022-12-15 20:55:21 +0100

    improved performance quite a bit (although i'm unsure how much array is doing)

--- 2022/day15/solution.py
+++ 2022/day15/solution.py
@@ -1,6 +1,7 @@
 """day n"""
 
 # common imports
+import array
 import re
 
 with open("input") as f:
@@ -11,7 +12,9 @@ MAX_COORD = 4000000
 
 
 def manhattan_dist(v1, v2):
-    return sum(abs(xi - yi) for (xi, yi) in zip(v1, v2))
+    (x1, x2) = v1
+    (y1, y2) = v2
+    return abs(x1-y1) + abs(x2-y2)
 
 
 coords = []
@@ -71,21 +74,29 @@ res -= len(set(b for (s, b) in coords if b[1] == YVALUE))
 print(f"part 1: {res}")
 
 # part 2
+
 res = 0
-tiles_to_check = [((0, 0), (MAX_COORD, MAX_COORD))]
+tiles_to_check_sx = array.array('l', [0])
+tiles_to_check_sy = array.array('l', [0])
+tiles_to_check_ex = array.array('l', [MAX_COORD])
+tiles_to_check_ey = array.array('l', [MAX_COORD])
+
 tiles_excluded = 0
 i = 0
 
 while True:
     i += 1
-    if i % 10000 == 0:
+    if i % 100000 == 0:
         print(f"{tiles_excluded / (MAX_COORD**2) * 100}")
 
-    if not tiles_to_check:
+    if not tiles_to_check_sx:
         print("no solution found")
         break
 
-    ((sx, sy), (ex, ey)) = tiles_to_check.pop()
+    sx = tiles_to_check_sx.pop()
+    sy = tiles_to_check_sy.pop()
+    ex = tiles_to_check_ex.pop()
+    ey = tiles_to_check_ey.pop()
 
     # is there any sensor that eliminates the entire tile? then False.
     # faster than nested for loops, apparently
@@ -108,9 +119,9 @@ while True:
             midpoint_x = (sx + ex) // 2
             midpoint_y = (sy + ey) // 2
 
-            tiles_to_check.append(((sx, sy), (midpoint_x, midpoint_y)))
-            tiles_to_check.append(((sx, midpoint_y + 1), (midpoint_x, ey)))
-            tiles_to_check.append(((midpoint_x + 1, sy), (ex, midpoint_y)))
-            tiles_to_check.append(((midpoint_x + 1, midpoint_y + 1), (ex, ey)))
+            tiles_to_check_sx.fromlist([sx, sx, midpoint_x+1, midpoint_x+1])
+            tiles_to_check_sy.fromlist([sy, midpoint_y+1, sy, midpoint_y+1])
+            tiles_to_check_ex.fromlist([midpoint_x, midpoint_x, ex, ex])
+            tiles_to_check_ey.fromlist([midpoint_y, ey, midpoint_y, ey])
 
 print(f"part 2: {res}")

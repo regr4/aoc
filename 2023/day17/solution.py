e39f14967e35e2ad3c52c4e2bb8fe9295099ff59
regr4 <60316183+regr4@users.noreply.github.com>
2023-12-17 23:54:55 +0100

    Combined 17 into one file, optimised it (algorithm is still awful though)

--- 2023/day17/solution.py
+++ 2023/day17/solution.py
@@ -1,83 +1,63 @@
 """day n"""
 
 from copy import deepcopy
-import itertools
-from operator import add
-from pprint import pprint
-import re
 
 with open("input") as f:
     inp = [[int(c) for c in l] for l in f.read().splitlines()]
 
-# part 1
 w = len(inp[0])
 h = len(inp)
 
 
-def ib(tile):
-    i, j = tile
-    return 0 <= i < w and 0 <= j < h
-
-
-def itr():
+def tiles():
     for j in range(h):
         for i in range(w):
             yield (i, j)
 
 
-def ix(grid, tile):
-    i, j = tile
-    return grid[j][i]
+HORI = (1, 0)
+VERT = (0, 1)
+
+LARGE = 2**20
 
 
-def orths(dirv):
-    return (tuple(reversed(dirv)), tuple(map(lambda x: -x, reversed(dirv))))
+def solve(lo, hi):
+    grids = {
+        HORI: [[LARGE for i in range(w)] for j in range(h)],
+        VERT: [[LARGE for i in range(w)] for j in range(h)],
+    }
+    grids[HORI][-1][-1] = grids[VERT][-1][-1] = 0
+
+    while True:
+        # Maybe: only check those that could possibly be affected
+        # (iterate over affected and adjust any ones that need it)
+        affected = set()
+
+        for di, grid in grids.items():
+            for tile in tiles():
+                for m in (1, -1):
+                    wt = 0
+                    for l in range(1, hi):
+                        newp = (tile[0] + m * l * di[0], tile[1] + m * l * di[1])
+                        if not (0 <= newp[0] < w and 0 <= newp[1] < h):
+                            break
+                        wt += inp[newp[1]][newp[0]]
+                        if l < lo:
+                            continue
+                        if (oth := wt + grids[(di[1], di[0])][newp[1]][newp[0]]) < (
+                            grid[tile[1]][tile[0]]
+                        ):
+                            grid[tile[1]][tile[0]] = oth
+                            affected.add((tile, di))
+        if not affected:
+            break
+    return grids
 
 
-# Try something like dynamic programming?
-
-UP = (0, -1)
-DOWN = (0, 1)
-LEFT = (-1, 0)
-RIGHT = (1, 0)
-
-LARGE = 2**100
-grids = {
-    UP: [[LARGE for i in range(w)] for j in range(h)],
-    DOWN: [[LARGE for i in range(w)] for j in range(h)],
-    LEFT: [[LARGE for i in range(w)] for j in range(h)],
-    RIGHT: [[LARGE for i in range(w)] for j in range(h)],
-}
-
-grids[UP][-1][-1] = inp[-1][-1]
-grids[RIGHT][-1][-1] = inp[-1][-1]
-grids[LEFT][-1][-1] = inp[-1][-1]
-grids[DOWN][-1][-1] = inp[-1][-1]
-
-
-while True:
-    newgrids = deepcopy(grids)
-
-    for di, grid in newgrids.items():
-        for tile in itr():
-            wt = 0
-            for l in range(1, 4):
-                newp = tuple(map(lambda x, y: x + l * y, tile, di))
-                if not ib(newp):
-                    break
-                wt += ix(inp, newp)
-                grid[tile[1]][tile[0]] = min(
-                    ix(grid, tile), *(wt + ix(grids[o], newp) for o in orths(di))
-                )
-
-    if grids == newgrids:
-        break
-    grids = newgrids
-
-res = min(x[0][0] for x in grids.values()) - inp[-1][-1]
-
-
+# part 1
+res = min(x[0][0] for x in solve(1, 4).values())
 print(f"part 1: {res}")
 
 # part 2
-print(f"part 2: {None}")
+res2 = min(x[0][0] for x in solve(4, 11).values())
+print(f"part 2: {res2}")
